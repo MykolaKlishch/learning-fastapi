0e6073fa0834df532cc30152dce8837f3aa32b01
mykola <mklishch@visualfabriq.com>
2022-07-15 22:21:17 +0300

    edited database config

--- database.py
+++ database.py
@@ -2,18 +2,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1111@localhost:5432/TodoApplicationDatabase'
+# MYSQL Series
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoapp"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+# MYSQL Series
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL
+# )
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
